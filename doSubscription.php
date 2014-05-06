bebede383cbe278e66134d828429085987858f61
Amit Sharma <root@domU-12-31-39-10-68-7B.compute-1.internal>
2014-05-06 22:44:12 +0530

    SubscribeURL Corrected

--- doSubscription.php
+++ doSubscription.php
@@ -7,7 +7,7 @@ use Aws\Sns\SnsClient;
 $region = $argv[1];
 $topicARN = $argv[2];
 $subscriberEndpoint = file_get_contents("http://169.254.169.254/latest/meta-data/public-hostname/");
-$subscriberEndpoint = $subscriberEndpoint . "/oneClickHttpSubscribe/getNotified.php";
+$subscriberEndpoint = "http://" . $subscriberEndpoint . "/oneClickHttpSubscribe/getNotified.php";
 
 $client = SnsClient::factory(array(
     'region' => $region, // (e.g., us-west-2)
